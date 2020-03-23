5337bd856ef3d4ce72c1727aed1739d84b11f256
nfebrian13 <febrian.nana@gmail.com>
2020-03-23 20:08:07 +0700

    autowired advanced

--- InversionControlDemo/src/main/java/com/nana/profiles/config/HelloConfig.java
+++ InversionControlDemo/src/main/java/com/nana/profiles/config/HelloConfig.java
@@ -1,7 +1,11 @@
 package com.nana.profiles.config;
 
+import java.util.jar.Attributes.Name;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import com.nana.profiles.services.HelloWorldFactory;
@@ -17,34 +21,51 @@ public class HelloConfig {
 
 	@Bean
 	@Profile("english")
-	public HelloWorldService helloWorldServiceEnglishImpl(HelloWorldFactory factory) {
+	@Primary
+	public HelloWorldService helloWorldServiceEnglish(HelloWorldFactory factory) {
 		return factory.createHelloWorldService("en");
 	}
 
 	@Bean
 	@Profile("spanish")
-	public HelloWorldService helloWorldServiceSpanishImpl(HelloWorldFactory factory) {
+	@Primary
+	public HelloWorldService helloWorldServiceSpanish(HelloWorldFactory factory) {
 		return factory.createHelloWorldService("es");
 	}
-	
+
+	@Bean(name = "french")
+	public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory) {
+		return factory.createHelloWorldService("fr");
+	}
+
+	@Bean
+	public HelloWorldService helloWorldServiceGerany(HelloWorldFactory factory) {
+		return factory.createHelloWorldService("de");
+	}
+
+	@Bean
+	public HelloWorldService helloWorldServicePoland(HelloWorldFactory factory) {
+		return factory.createHelloWorldService("pl");
+	}
+
 	@Bean
 	@Profile("russian")
-	public HelloWorldService helloWorldServiceRussianImpl(HelloWorldFactory factory) {
+	public HelloWorldService helloWorldServiceRussian(HelloWorldFactory factory) {
 		return factory.createHelloWorldService("ru");
 	}
 
 	/*
 	 * @Bean
 	 * 
-	 * @Profile("english") public HelloWorldService helloWorldServiceEnglishImpl() {
-	 * return new HelloWorldServiceEnglishImpl(); }
+	 * @Profile("english") public HelloWorldService helloWorldServiceEnglish() {
+	 * return new HelloWorldServiceEnglish(); }
 	 */
 
 	/*
 	 * @Bean
 	 * 
-	 * @Profile("spanish") public HelloWorldService helloWorldServiceSpanishImpl() {
-	 * return new HelloWorldServiceSpanishImpl(); }
+	 * @Profile("spanish") public HelloWorldService helloWorldServiceSpanish() {
+	 * return new HelloWorldServiceSpanish(); }
 	 */
 
 }
